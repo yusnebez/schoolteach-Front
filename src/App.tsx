a818c42f251303d01a1b84653c5693e194428541
yusnebez <yusnebez@gmail>
2021-09-05 12:41:15 +0100

    proyecto creado y preparado

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import Parrafo from './components/Parrafo';
+
+function App() {
+  return (
+    <div className="App">
+     <Parrafo/>
+    </div>
+  );
+}
+
+export default App;
